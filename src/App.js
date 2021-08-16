cf10219deb37a25b8e16bd22b79ae529aa1d1571
JP Eiler <jp.eiler97@gmail.com>
2021-08-16 11:12:23 -0500

    created basic login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
-import HelloWorld from "./components/HelloWorld";
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const admin = {
+    email: "admin@admin.com",
+    password: "qqq",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (details.email === admin.email && details.password === admin.password) {
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not match");
+      setError("Details do not match");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
+
   return (
-    <h1>
-      <HelloWorld></HelloWorld>
-    </h1>
+    <div className="App">
+      {user.email !== "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error}></LoginForm>
+      )}
+    </div>
   );
 }
 
